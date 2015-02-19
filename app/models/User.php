af411a04d434d2b74d0d5f8d9c5d86e8ec503326
Andrea Gaither <angaither@gmail.com>
2015-02-19 15:28:17 -0500

    Don't require passwords on validation. (Some apps don't have passwords) Fixes #51

--- app/models/User.php
+++ app/models/User.php
@@ -33,8 +33,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
    */
   private $rules = array(
       'email'=>'email|unique:users',
-      'mobile'=>'unique:users',
-      'password'=>'required'
+      'mobile'=>'unique:users'
   );
 
   /**
