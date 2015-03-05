c3e4ffaadb8ecaa250cd4c17236c2bcafd7d80e3
Morgan Rich <mrich@dosomething.org>
2015-03-05 14:56:41 -0500

    Adding gateway user

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ server  ENV["SERVER_NAME"], :app, :web
 set :user, "dosomething"
 set :group, "dosomething"
 set :use_sudo, false
-set :gateway, "ec2-52-1-92-62.compute-1.amazonaws.com"
+set :gateway, "ec2-user@ec2-52-1-92-62.compute-1.amazonaws.com"
 
 set :repository, "."
 set :scm, :none
