eace787f7d7e0b8f402cb5c40420ae83eb76e70b
Andrea Gaither <angaither@gmail.com>
2015-04-08 17:24:44 -0400

    Added drupal to non-local services config.

--- app/config/services.php
+++ app/config/services.php
@@ -2,30 +2,34 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Third Party Services
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storing the credentials for third party services such
-	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
-	| default location for this type of information, allowing packages
-	| to have a conventional place to find your various credentials.
-	|
-	*/
+  /*
+  |--------------------------------------------------------------------------
+  | Third Party Services
+  |--------------------------------------------------------------------------
+  |
+  | This file is for storing the credentials for third party services such
+  | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+  | default location for this type of information, allowing packages
+  | to have a conventional place to find your various credentials.
+  |
+  */
 
-	'mailgun' => array(
-		'domain' => '',
-		'secret' => '',
-	),
+  'mailgun' => array(
+    'domain' => '',
+    'secret' => '',
+  ),
 
-	'mandrill' => array(
-		'secret' => '',
-	),
+  'mandrill' => array(
+    'secret' => '',
+  ),
 
-	'stripe' => array(
-		'model'  => 'User',
-		'secret' => '',
-	),
+  'stripe' => array(
+    'model'  => 'User',
+    'secret' => '',
+  ),
+  'drupal' => array(
+    'url' => getenv('DRUPAL_API_URL'),
+    'version' => 'v1',
+  ),
 
 );
