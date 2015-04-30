79590e52ae6f388ae881f28ae6c1a921bbe92210
David Furnes <david@dfurnes.com>
2015-04-30 11:26:30 -0400

    Clean up Drupal API constructor a wee bit.

--- app/Northstar/Services/Drupal.php
+++ app/Northstar/Services/Drupal.php
@@ -1,27 +1,26 @@
 <?php namespace Northstar\Services\Drupal;
 
+use GuzzleHttp\Client;
+use Config;
+
 class DrupalAPI {
 
   protected $client;
 
   public function __construct()
   {
-    $base_url = \Config::get('services.drupal.url');
-    if (\App::environment('local')) {
-      $base_url .=  ":" . \Config::get('services.drupal.port');
-    }
-    $base_url .= '/api';
-    $version = \Config::get('services.drupal.version');
-    $client = new \GuzzleHttp\Client([
-      'base_url' => [$base_url . '/{version}/', ['version' => $version]],
-      'defaults' => array(
+    $base_url = Config::get('services.drupal.url');
+    $version = Config::get('services.drupal.version');
+
+    $this->client = new Client([
+      'base_url' => [$base_url . '/api/{version}/', ['version' => $version]],
+      'defaults' => [
         'headers' => [
           'Content-Type' => 'application/json',
           'Accept' => 'application/json'
-          ]
-        ),
+        ]
+      ],
     ]);
-    $this->client = $client;
   }
 
   public function campaigns($id = NULL)
