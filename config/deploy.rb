3b2c4e6e35282bf684b687159bcd8ccf2fc438a2
Morgan Rich <mrich@dosomething.org>
2015-05-14 15:54:36 -0400

    Adding slack updates and changes to paths for L5

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,11 +27,11 @@ namespace :deploy do
   folders = %w{cache logs meta sessions system views}
 
   task :link_folders do
-    run "ln -nfs #{shared_path}/.env.php #{release_path}/"
-    run "ln -nfs #{shared_path}/content #{release_path}/public"
-    run "ln -nfs #{shared_path}/pages #{release_path}/public/pages"
+    run "ln -nfs #{shared_path}/.env #{release_path}/"
+    run "ln -nfs #{shared_path}/images #{release_path}/public"
+    run "ln -nfs #{shared_path}/thumbnails #{release_path}/public"
     folders.each do |folder|
-      run "ln -nfs #{shared_path}/#{folder} #{release_path}/app/storage/#{folder}"
+      run "ln -nfs #{shared_path}/#{folder} #{release_path}/storage/#{folder}"
     end
   end
 
