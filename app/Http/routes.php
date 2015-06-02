fe9d6956b53a9f54f9021894de42ae57f4829ed1
Chloe Lee <chloealessandra@gmail.com>
2015-06-02 13:25:44 -0400

    change from users/campaigns to user/campaigns

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
 Route::group(['prefix' => 'v1', 'middleware' => 'auth.api'], function () {
     // Campaigns.
     Route::group(['middleware' => 'auth.token'], function () {
-        Route::get('users/campaigns/{campaign_id}', 'CampaignController@show');
-        Route::post('users/campaigns/{campaign_id}/signup', 'CampaignController@signup');
-        Route::post('users/campaigns/{campaign_id}/reportback', 'CampaignController@reportback');
-        Route::put('users/campaigns/{campaign_id}/reportback', 'CampaignController@reportback');
+        Route::get('user/campaigns/{campaign_id}', 'CampaignController@show');
+        Route::post('user/campaigns/{campaign_id}/signup', 'CampaignController@signup');
+        Route::post('user/campaigns/{campaign_id}/reportback', 'CampaignController@reportback');
+        Route::put('user/campaigns/{campaign_id}/reportback', 'CampaignController@reportback');
     });
 
     // Sessions.
