4bd56cbb23e89feaf1b7ccc4916642b4ba253e1d
Shae Smith <ssmith@dosomething.org>
2015-06-11 16:38:16 -0400

    Send notifications to particular installation ids

--- app/Events/UserSignedUp.php
+++ app/Events/UserSignedUp.php
@@ -19,7 +19,6 @@ class UserSignedUp extends Event {
 		$this->drupal_id = $user->drupal_id;
 		$this->signup_id = $user->campaigns[0]->signup_id;
 		$this->signup_source = $user->campaigns[0]->signup_source;
-		//$this->installation_id = $user->installationid;
 	}
 
 }
