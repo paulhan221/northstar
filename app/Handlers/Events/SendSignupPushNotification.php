264eab69160e626571e185aeecf05a272444fc57
Shae Smith <ssmith@dosomething.org>
2015-06-12 10:17:04 -0400

    Add checks for empty values

--- app/Handlers/Events/SendSignupPushNotification.php
+++ app/Handlers/Events/SendSignupPushNotification.php
@@ -34,16 +34,20 @@ class SendSignupPushNotification {
     $group = User::where('campaigns', 'elemMatch', ['signup_id' => (int)$event->signup_id])
             ->orWhere('campaigns', 'elemMatch', ['signup_source' => $event->signup_id])->get();
 
-    // Loop through the users in the group.
-    foreach ($group as $user) {
-      $drupal_id = $user->drupal_id;
-      // Check that this user is not the user that triggered the event.
-      if ($drupal_id !== $event->drupal_id) {
-        // @TODO - This is placeholder content.
-        $data = array("alert" => "I just signed up to your group");
+    if (count($group) > 0) {
+      // Loop through the users in the group.
+      foreach ($group as $user) {
+        $drupal_id = $user->drupal_id;
+        // Check that this user is not the user that triggered the event.
+        if ($drupal_id !== $event->drupal_id) {
+          // @TODO - This is placeholder content.
+          $data = array("alert" => "I just signed up to your group");
 
-        // Send notifications to the users devices.
-        $this->parse->sendPushNotification($user->parse_installation_ids, $data);
+          // Send notifications to the users devices.
+          if (!empty($user->parse_installation_ids)) {
+            $this->parse->sendPushNotification($user->parse_installation_ids, $data);
+          }
+        }
       }
     }
   }
