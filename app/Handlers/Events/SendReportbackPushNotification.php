a33d0f569f3a7e67884f550b0dbde4da9e6d4db3
Jon Uy <jonxuy@gmail.com>
2015-07-17 06:54:36 -0400

    Code style clean up on SendReportbackPushNotification.php - 4 space indents

--- app/Handlers/Events/SendReportbackPushNotification.php
+++ app/Handlers/Events/SendReportbackPushNotification.php
@@ -8,48 +8,46 @@ use Northstar\Services\Parse;
 
 class SendReportbackPushNotification {
 
-  /**
-   * Parse API wrapper.
-   * @var Parse
-   */
-  protected $parse;
+    /**
+     * Parse API wrapper.
+     * @var Parse
+     */
+    protected $parse;
 
-  /**
-   * Create the event handler.
-   *
-   * @return void
-   */
-  public function __construct(Parse $parse)
-  {
-    $this->parse = $parse;
-  }
-
-  /**
-   * Handle the event.
-   *
-   * @param  UserReportedBack  $event
-   * @return void
-   */
-  public function handle(UserReportedBack $event)
-  {
-
-    $group = User::group($event->campaign->signup_id);
-
-    if (count($group) > 0) {
-      // Loop through the users in the group.
-      foreach ($group as $user) {
-        $drupal_id = $user->drupal_id;
-        // Check that this user is not the user that triggered the event.
-        if ($drupal_id !== $event->user->drupal_id) {
-          // @TODO - This is placeholder content.
-          $data = array("alert" => "A user in your group just reported back");
-
-          // Send notifications to the users devices.
-          if (!empty($user->parse_installation_ids)) {
-            $this->parse->sendPushNotification($user->parse_installation_ids, $data);
-          }
-        }
-      }
+    /**
+     * Create the event handler.
+     *
+     * @return void
+     */
+    public function __construct(Parse $parse)
+    {
+        $this->parse = $parse;
     }
-  }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserReportedBack  $event
+     * @return void
+     */
+    public function handle(UserReportedBack $event)
+    {
+
+        $group = User::group($event->campaign->signup_id);
+
+        if (count($group) > 0) {
+            // Loop through the users in the group.
+            foreach ($group as $user) {
+                $drupal_id = $user->drupal_id;
+                // Check that this user is not the user that triggered the event.
+                if ($drupal_id !== $event->user->drupal_id && !empty($user->parse_installation_ids)) {
+                    // @TODO - This is placeholder content.
+                    $data = array("alert" => "A user in your group just reported back");
+
+                    // Send notifications to the user's devices.
+                    $this->parse->sendPushNotification($user->parse_installation_ids, $data);
+                }
+            }
+        }
+    }
 }
